150c86f2b84de29bb2d95c68d93bd45dab98613f
Afif Alfiano <afif.alfiano@uii.ac.id>
2020-11-04 18:12:44 +0700

    implements workbox

--- js/db.js
+++ js/db.js
@@ -1,4 +1,4 @@
-var dbPromised = idb.open("kabarbola", 1, function(upgradeDb) {
+var dbPromised = idb.open("kabarbola", 1, (upgradeDb) => {
     var teamObjectStore = upgradeDb.createObjectStore("teams", {
         keyPath: "id"
     });
@@ -8,69 +8,84 @@ var dbPromised = idb.open("kabarbola", 1, function(upgradeDb) {
 function checkData(team) {
     return new Promise((resolve, reject) => {
         dbPromised
-        .then(function(db) {
+        .then((db) => {
             var tx = db.transaction("teams", "readonly");
             var store = tx.objectStore("teams");
             return store.get(team.id);
         })
-        .then(function(data){
+        .then((data) => {
             console.log(data);
             resolve(data);
         })
-    })
-    
+        .catch(err => {
+            console.log('Error: '+ err);
+        })
+    })    
 }
 
 function saveForLater(team) {
     dbPromised
-    .then(function(db) {
+    .then((db) => {
         var tx = db.transaction("teams", "readwrite");
         var store = tx.objectStore("teams");
         console.log(team);
-        store.add(team);
+        store.put(team);
         return tx.complete;
     })
-    .then(function() {
+    .then(() => {
         M.toast({html: 'Tim Berhasil disimpan!'});
+    })
+    .catch(err => {
+        console.log('Error: '+ err);
     })
 }
 
 function deleteItem(team) {
-    dbPromised.then(function(db) {
+    dbPromised.then((db) => {
         var tx = db.transaction('teams', "readwrite");
         var store = tx.objectStore("teams");
         store.delete(team.id);
         return tx.complete;
-    }).then(function() {
+    }).then(() => {
         M.toast({html: 'Tim Berhasil dihapus!'});
+    })
+    .catch(err => {
+        console.log('Error: '+ err);
     })
 }
 
 function getAll() {
     return new Promise((resolve, reject) => {
         dbPromised
-        .then(function(db) {
+        .then((db) => {
             var tx = db.transaction("teams", "readonly");
             var store = tx.objectStore("teams");
             return store.getAll();
         })
-        .then(function(team) {
+        .then((team) => {
             resolve(team)
         })
+        .catch(err => {
+            console.log('Error: '+ err);
+        })
     })
+    
 }
 
 function getById(id) {
     return new Promise(function(resolve, reject) {
       dbPromised
-        .then(function(db) {
+        .then((db) => {
           var tx = db.transaction("teams", "readonly");
           var store = tx.objectStore("teams");
           return store.get(id);
         })
-        .then(function(team) {
+        .then((team) => {
           console.log(team);
           resolve(team);
-        });
+        })
+        .catch(err => {
+            console.log('Error: '+ err);
+        })
     });
   }
---
+++ js/tim.js
@@ -0,0 +1,60 @@
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function() {
+      navigator.serviceWorker
+        .register("/sw.js")
+        .then(function() {
+          console.log("Pendaftaran ServiceWorker berhasil");
+        })
+        .catch(function() {
+          console.log("Pendaftaran ServiceWorker gagal");
+        });
+    });
+  } else {
+    console.log("ServiceWorker belum didukung browser ini.");
+  }
+
+document.addEventListener("DOMContentLoaded", function() {
+var urlParams = new URLSearchParams(window.location.search);
+var isFromSaved = urlParams.get("timfavorit");
+var save = document.getElementById("save");
+var del = document.getElementById("delete");
+
+if (isFromSaved) {
+    // Hide fab jika dimuat dari indexed db
+    save.style.display = 'none';
+    del.style.display = 'none';
+    
+} else {
+    var item = getTeamById();
+}
+
+item.then(function(tim) {
+  checkData(tim).then(function(cek) {
+    console.log(cek);
+    if (cek === null || cek === undefined) {
+      del.style.display = 'none';
+      save.onclick = function() {
+            console.log("Tombol FAB di klik.");
+              saveForLater(tim);
+              setTimeout(
+                  () => {
+                      location.reload(true);
+                  }, 2000
+              )
+      };
+    } else {
+      save.style.display = 'none';
+      del.onclick = function() {
+          console.log('Tombol Delete di klik');
+          deleteItem(tim);
+          setTimeout(
+            () => {
+                location.reload(true);
+            }, 2000
+        )
+      }
+    }
+  })
+  
+});
+});
